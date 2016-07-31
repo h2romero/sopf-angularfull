efad23cc4f725554c375cdedaf8e25765b8ab6fc
Hector Romero <hectorromero@Hectors-MacBook-Pro.local>
2016-07-30 22:37:12 -0700

    added transaction owner

--- client/app/transaction/transaction-add.js
+++ client/app/transaction/transaction-add.js
@@ -8,10 +8,11 @@
  * Controller of the clientApp
  */
 angular.module('sopfApp')
-  .controller('TransactionAddCtrl', function ($scope, $http, $location, socket) {
+  .controller('TransactionAddCtrl', function ($scope, $http, $location, socket, Auth) {
     var vm = this;
     vm.transaction = {};
     vm.transaction.dueDate = Date.now();
+    vm.transaction.owner = Auth.getCurrentUser()._id;
     vm.saveTransaction = function() {
       //Transaction.post(vm.transaction).then(function() {
       //  $location.path('/transactions');
