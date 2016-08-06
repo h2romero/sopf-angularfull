f3b24660e8711e98d10899bfb8ca6eee0f34c5fb
Hector Romero <hectorromero@Hectors-MacBook-Pro.local>
2016-08-06 09:18:58 -0700

    added x-editable

--- client/app/app.js
+++ client/app/app.js
@@ -6,7 +6,8 @@ angular.module('sopfApp', [
   'ngSanitize',
   'btford.socket-io',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'xeditable'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
@@ -15,6 +16,12 @@ angular.module('sopfApp', [
     $locationProvider.html5Mode(true);
     $httpProvider.interceptors.push('authInterceptor');
   })
+  .config(["$httpProvider", function ($httpProvider) {
+    $httpProvider.defaults.transformResponse.push(function (responseData) {
+      convertDateStringsToDates(responseData);
+      return responseData;
+    });
+  }])
 
   .factory('authInterceptor', function ($rootScope, $q, $cookieStore, $location) {
     return {
@@ -52,4 +59,38 @@ angular.module('sopfApp', [
         }
       });
     });
-  });
+  })
+
+.run(function(editableOptions) {
+  editableOptions.theme = 'bs3';
+});
+
+//var regexIso8601 = /^20[\d]{2}(\/|-)[\d]{2}(\/|-)[\d]{2}(\s|T)[\d]{2}:[\d]{2}:[\d]{2}((\+|-)[0-1][\d]:?(0|3)0)?$/;
+var regexIso8601 = /^\d{4}(-\d\d(-\d\d(T\d\d:\d\d(:\d\d)?(\.\d+)?(([+-]\d\d:\d\d)|Z)?)?)?)?$/i;
+
+function convertDateStringsToDates(input) {
+  // Ignore things that aren't objects.
+  if (typeof input !== "object") return input;
+
+  for (var key in input) {
+    if (!input.hasOwnProperty(key)) continue;
+
+    var value = input[key];
+    var match;
+    // Check for string properties which look like dates.
+    // TODO: Improve this regex to better match ISO 8601 date strings.
+    if (typeof value === "string" && (match = value.match(regexIso8601))) {
+      // Assume that Date.parse can parse ISO 8601 strings, or has been shimmed in older browsers to do so.
+      var milliseconds = Date.parse(match[0]);
+      if (!isNaN(milliseconds)) {
+        var localDate = new Date(milliseconds);
+        var localTime = localDate.getTime();
+        var localOffset = localDate.getTimezoneOffset() * 60000;
+        input[key] = new Date(localTime + localOffset);
+      }
+    } else if (typeof value === "object") {
+      // Recurse into object
+      convertDateStringsToDates(value);
+    }
+  }
+}
--- client/app/transaction/transaction.controller.js
+++ client/app/transaction/transaction.controller.js
@@ -1,13 +1,19 @@
 'use strict';
 
 angular.module('sopfApp')
-  .controller('TransactionCtrl', function ($scope, $http, socket, Auth) {
+  .controller('TransactionCtrl', function ($scope, $http, $stateParams, $location, socket, Auth) {
     var vm = this;
     $http.get('/api/transactions/' + Auth.getCurrentUser()._id).success(function(transactions) {
     //$http.get('/api/transactions').success(function(transactions) {
       vm.transactions = transactions;
       socket.syncUpdates('transaction', vm.transactions);
     });
+
+  vm.saveTransaction = function (transaction) {
+    $http.put('/api/transactions/' + transaction._id, transaction).then(function() {
+      $location.path('/transactions');
+    });
+  }
 
     $scope.$on('$destroy', function () {
       socket.unsyncUpdates('transaction');
