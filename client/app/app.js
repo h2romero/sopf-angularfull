271273945e514c90a67945b3fcffd7bf50cdbcde
Hector Romero <hectorromero@Hectors-MacBook-Pro.local>
2016-08-17 23:20:02 -0700

    moved convertDateStringsToDates to make date formatting work for x-editable

--- client/app/app.js
+++ client/app/app.js
@@ -22,6 +22,35 @@ angular.module('sopfApp', [
       convertDateStringsToDates(responseData);
       return responseData;
     });
+
+    var regexIso8601 = /^\d{4}(-\d\d(-\d\d(T\d\d:\d\d(:\d\d)?(\.\d+)?(([+-]\d\d:\d\d)|Z)?)?)?)?$/i;
+
+    function convertDateStringsToDates(input) {
+      // Ignore things that aren't objects.
+      if (typeof input !== "object") return input;
+
+      for (var key in input) {
+        if (!input.hasOwnProperty(key)) continue;
+
+        var value = input[key];
+        var match;
+        // Check for string properties which look like dates.
+        // TODO: Improve this regex to better match ISO 8601 date strings.
+        if (typeof value === "string" && (match = value.match(regexIso8601))) {
+          // Assume that Date.parse can parse ISO 8601 strings, or has been shimmed in older browsers to do so.
+          var milliseconds = Date.parse(match[0]);
+          if (!isNaN(milliseconds)) {
+            var localDate = new Date(milliseconds);
+            var localTime = localDate.getTime();
+            var localOffset = localDate.getTimezoneOffset() * 60000;
+            input[key] = new Date(localTime + localOffset);
+          }
+        } else if (typeof value === "object") {
+          // Recurse into object
+          convertDateStringsToDates(value);
+        }
+      }
+    }
   }])
 
   .factory('authInterceptor', function ($rootScope, $q, $cookieStore, $location) {
@@ -65,34 +94,4 @@ angular.module('sopfApp', [
 .run(function(editableOptions) {
   editableOptions.theme = 'bs3';
 });
-
-//var regexIso8601 = /^20[\d]{2}(\/|-)[\d]{2}(\/|-)[\d]{2}(\s|T)[\d]{2}:[\d]{2}:[\d]{2}((\+|-)[0-1][\d]:?(0|3)0)?$/;
-var regexIso8601 = /^\d{4}(-\d\d(-\d\d(T\d\d:\d\d(:\d\d)?(\.\d+)?(([+-]\d\d:\d\d)|Z)?)?)?)?$/i;
-
-function convertDateStringsToDates(input) {
-  // Ignore things that aren't objects.
-  if (typeof input !== "object") return input;
-
-  for (var key in input) {
-    if (!input.hasOwnProperty(key)) continue;
-
-    var value = input[key];
-    var match;
-    // Check for string properties which look like dates.
-    // TODO: Improve this regex to better match ISO 8601 date strings.
-    if (typeof value === "string" && (match = value.match(regexIso8601))) {
-      // Assume that Date.parse can parse ISO 8601 strings, or has been shimmed in older browsers to do so.
-      var milliseconds = Date.parse(match[0]);
-      if (!isNaN(milliseconds)) {
-        var localDate = new Date(milliseconds);
-        var localTime = localDate.getTime();
-        var localOffset = localDate.getTimezoneOffset() * 60000;
-        input[key] = new Date(localTime + localOffset);
-      }
-    } else if (typeof value === "object") {
-      // Recurse into object
-      convertDateStringsToDates(value);
-    }
-  }
-}
 
