81e74401fcd9d1e4f0b3cead246ea083ed6af752
Hector Romero <hectorromero@Hectors-MacBook-Pro.local>
2016-08-17 23:44:15 -0700

    try new date formatting to fix app in openshift

--- client/app/app.js
+++ client/app/app.js
@@ -37,14 +37,15 @@ angular.module('sopfApp', [
         // Check for string properties which look like dates.
         // TODO: Improve this regex to better match ISO 8601 date strings.
         if (typeof value === "string" && (match = value.match(regexIso8601))) {
-          // Assume that Date.parse can parse ISO 8601 strings, or has been shimmed in older browsers to do so.
-          var milliseconds = Date.parse(match[0]);
-          if (!isNaN(milliseconds)) {
-            var localDate = new Date(milliseconds);
-            var localTime = localDate.getTime();
-            var localOffset = localDate.getTimezoneOffset() * 60000;
-            input[key] = new Date(localTime + localOffset);
-          }
+          // // Assume that Date.parse can parse ISO 8601 strings, or has been shimmed in older browsers to do so.
+          // var milliseconds = Date.parse(match[0]);
+          // if (!isNaN(milliseconds)) {
+          //   var localDate = new Date(milliseconds);
+          //   var localTime = localDate.getTime();
+          //   var localOffset = localDate.getTimezoneOffset() * 60000;
+          //   input[key] = new Date(localTime + localOffset);
+          // }
+          input[key] = new Date(match[0]);
         } else if (typeof value === "object") {
           // Recurse into object
           convertDateStringsToDates(value);
