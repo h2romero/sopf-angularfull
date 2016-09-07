2872212d7bed90772ea8a583f67b6386fa6b249e
Hector Romero <hectorromero@Hectors-MacBook-Pro.local>
2016-09-06 22:36:52 -0700

    remove comments from period to fix grunt build issues

--- client/app/app.js
+++ client/app/app.js
@@ -23,36 +23,6 @@ angular.module('sopfApp', [
       convertDateStringsToDates(responseData);
       return responseData;
     });
-
-    // var regexIso8601 = /^\d{4}(-\d\d(-\d\d(T\d\d:\d\d(:\d\d)?(\.\d+)?(([+-]\d\d:\d\d)|Z)?)?)?)?$/i;
-    var regexIso8601 = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
-    function convertDateStringsToDates(input) {
-      // Ignore things that aren't objects.
-      if (typeof input !== "object") return input;
-
-      for (var key in input) {
-        if (!input.hasOwnProperty(key)) continue;
-
-        var value = input[key];
-        var match;
-        // Check for string properties which look like dates.
-        // TODO: Improve this regex to better match ISO 8601 date strings.
-        if (typeof value === "string" && (match = value.match(regexIso8601))) {
-          // // Assume that Date.parse can parse ISO 8601 strings, or has been shimmed in older browsers to do so.
-          // var milliseconds = Date.parse(match[0]);
-          // if (!isNaN(milliseconds)) {
-          //   var localDate = new Date(milliseconds);
-          //   var localTime = localDate.getTime();
-          //   var localOffset = localDate.getTimezoneOffset() * 60000;
-          //   input[key] = new Date(localTime + localOffset);
-          // }
-          input[key] = new Date(match[0]);
-        } else if (typeof value === "object") {
-          // Recurse into object
-          convertDateStringsToDates(value);
-        }
-      }
-    }
   }])
 
   .factory('authInterceptor', function ($rootScope, $q, $cookieStore, $location) {
@@ -93,7 +63,37 @@ angular.module('sopfApp', [
     });
   })
 
-.run(function(editableOptions) {
-  editableOptions.theme = 'bs3';
-});
+  .run(function(editableOptions) {
+    editableOptions.theme = 'bs3';
+  });
+
+//var regexIso8601 = /^20[\d]{2}(\/|-)[\d]{2}(\/|-)[\d]{2}(\s|T)[\d]{2}:[\d]{2}:[\d]{2}((\+|-)[0-1][\d]:?(0|3)0)?$/;
+var regexIso8601 = /^\d{4}(-\d\d(-\d\d(T\d\d:\d\d(:\d\d)?(\.\d+)?(([+-]\d\d:\d\d)|Z)?)?)?)?$/i;
+
+function convertDateStringsToDates(input) {
+  // Ignore things that aren't objects.
+  if (typeof input !== "object") return input;
+
+  for (var key in input) {
+    if (!input.hasOwnProperty(key)) continue;
+
+    var value = input[key];
+    var match;
+    // Check for string properties which look like dates.
+    // TODO: Improve this regex to better match ISO 8601 date strings.
+    if (typeof value === "string" && (match = value.match(regexIso8601))) {
+      // Assume that Date.parse can parse ISO 8601 strings, or has been shimmed in older browsers to do so.
+      var milliseconds = Date.parse(match[0]);
+      if (!isNaN(milliseconds)) {
+        var localDate = new Date(milliseconds);
+        var localTime = localDate.getTime();
+        var localOffset = localDate.getTimezoneOffset() * 60000;
+        input[key] = new Date(localTime + localOffset);
+      }
+    } else if (typeof value === "object") {
+      // Recurse into object
+      convertDateStringsToDates(value);
+    }
+  }
+}
 
